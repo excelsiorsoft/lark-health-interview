6d5272091acf4a1dbda87ccb7bd3932bd9522e8d
sleyzerzon <senya22@yahoo.com>
2021-06-21 15:46:12 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,39 @@
+**Take Home Assignment**
+
+**Task**
+
+    /* Given a stream of items, represented as one item at a time, be able to print the top 3 occurances at any time within a window of 3 items, ordered by frequency count in 
+    
+          A B B C A
+           |_____| // line 14     
+    
+     i.e.:
+        
+         A -> 1 A
+         B -> 1 A, 1 B // ties can be in any order
+         B -> 2 B, 1 A // ordered by frequency count in descending order
+         C -> 2 B, 1 C // the first A is now outside the sliding window
+         A -> 1 B, 1 A, 1 C
+    
+    
+    const print = function() {
+      console.log("test print")
+    }
+    
+    const add = function(item) {
+      console.log("test add()")  
+    
+    }
+    
+    add("A")
+    print()
+    add("B")
+    print()
+    add("B")
+    print()
+    add("C")
+    print()
+    add("A")
+    print()
+    
+    */
---
+++ sliding-window/src/Main.java
@@ -0,0 +1,143 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// Need to deal with sort order
+class Main {
+
+	private static Map<String, Integer> stats = new HashMap<>();
+	private static String[] window = new String[3];
+	private static long counter = 0;
+
+	public static void main(String[] args) {
+
+		Main main = new Main();
+
+	
+		main.add("A");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("C");
+		main.print();
+		main.add("A");
+		main.print();
+
+	
+/*		
+		main.add("A");
+		main.print();
+		main.add("A");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("A");
+		main.print();
+		main.add("A");
+		main.print();
+		main.add("C");
+		main.print();
+*/
+		
+/*		
+		main.add("A");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("C");
+		main.print();
+		main.add("A");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("C");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+*/
+	
+/*		
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("C");
+		main.print();
+		main.add("B");
+		main.print();
+		main.add("D");
+		main.print();
+		main.add("K");
+		main.print();
+*/		
+	}
+
+	public void print() {
+
+		Map<String, Integer> sortedMapReverseOrder = stats.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+		System.out.println(sortedMapReverseOrder);
+	}
+
+
+	private static void add(String item) {
+		int position = 0;
+		
+		if (0 <= counter && counter < window.length) {
+			position = (int) counter;
+		} else {
+			position = (int) counter % window.length;
+			
+			String keyToReplace = window[position];
+			
+			Integer occOfReplacement = stats.get(keyToReplace);
+			if (occOfReplacement !=null) {
+				if(occOfReplacement.intValue() == 1) {
+					stats.remove(keyToReplace);
+				}else {
+					stats.put(keyToReplace,occOfReplacement-1);
+				}
+			}
+			
+		}
+		
+		window[position] = item;
+		
+		Integer occOfNew = stats.get(item);
+
+		if (occOfNew == null) {
+			stats.put(item, 1);
+		} else {
+			stats.put(item, occOfNew + 1);
+		}
+		
+		if(counter <= Long.MAX_VALUE-1) {
+			counter++;
+		}else {
+			counter=0;
+			position=0;
+		}
+
+		
+	}
+
+}
